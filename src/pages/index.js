541767b83fb4b914164ef99d619e0a51986c1ef4
Bryce Gibson <bgibson@limepoint.com>
2022-10-13 11:01:45 +1100

    Remove some of the default content+config.
    As part of the move to our own content.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ function HomepageHeader() {
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/docs/intro'>
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className='button button--secondary button--lg' to='/docs/'>
+            Learn more about OpsChain
           </Link>
         </div>
       </div>
@@ -27,7 +27,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description='Description will go into a meta tag in <head />'>
+    <Layout>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
