4b40d8f3695857643267a1cd4cc736ce6877905f
Bryce Gibson <bgibson@limepoint.com>
2022-10-14 14:09:47 +1100

    Use a png logo as the svg one is so large.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
         title: 'OpsChain',
         logo: {
           alt: 'OpsChain logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [],
       },
