712091b4d792eec2c07d2d64d1b153dfab2dc3e1
OpsChain <opschain@limepoint.com>
2025-09-11 15:32:33 +1000

    OpsChain publish edge docs - 2025-09-11.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons} style={{ marginBottom: '2em' }}>
-          <Link className='button button--secondary button--lg' to='/docs/'>
+          <Link className='button button--secondary button--lg' to='/docs/about/opschain'>
             Learn how to use OpsChain
           </Link>
         </div>
